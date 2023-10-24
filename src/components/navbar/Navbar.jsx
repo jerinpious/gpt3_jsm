7ce2d269d2ea780f4d93070324715ad24be9f3ae
jerinpious <jpious4925@conestogac.on.ca>
2023-10-24 18:58:16 -0400

    sections

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {RiMenuLine,RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links"></div>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>      
     </div>
   )
 }
