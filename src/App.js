791d723ff627c017754bd1925d90b777cbd04bd2
jerinpious <jpious4925@conestogac.on.ca>
2023-10-24 16:31:15 -0400

    components and containers with react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Footer, Header, Blog, Possibilty, Features, WhatGPT3} from './containers'
+import {CTA,Brand,Navbar} from './components'
+
+const App = () => {
+  return (
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './possibilty.css';
+const Possibilty = () => {
+  return (
+    <div>
+      Possibilty
+    </div>
+  )
+}
+
+export default Possibilty
