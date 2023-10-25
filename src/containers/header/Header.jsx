26e5f13084fd16f0a76b0893052824bc1d29cd86
jerinpious <jpious4925@conestogac.on.ca>
2023-10-25 01:57:37 -0400

    navbar completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import './header.css';
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="gpt__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Lets's Create Something Amazing with GPT-3 OpenAI</h1>
+          <div className="gpt3__header-content__input">
+            <p>Let's explore the new world of infinite knowledge and assistance with the use of GPT-3 OpenAI</p>
+            <input type="email" placeholder="Your Email Address" />
+            <button type='button'>Get Started</button>
+          </div>      
+      </div>
     </div>
   )
 }
