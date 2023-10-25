4dd60c3b821e7f0dd807b1c291d16485ba12b92a
jerinpious <jpious4925@conestogac.on.ca>
2023-10-25 02:06:05 -0400

    header jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 const Header = () => {
   return (
     <div className="gpt__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">Lets's Create Something Amazing with GPT-3 OpenAI</h1>
+          
           <div className="gpt3__header-content__input">
             <p>Let's explore the new world of infinite knowledge and assistance with the use of GPT-3 OpenAI</p>
             <input type="email" placeholder="Your Email Address" />
             <button type='button'>Get Started</button>
+          </div>
+          
+          <div className="gpt3__header-content_people">
+            <img src={people}  />
+            <p>1,600 people requested access</p>
+          </div>
+          
+          <div className="gpt3__header-img">
+            <img src={ai} alt="ai" />  
           </div>      
       </div>
     </div>
